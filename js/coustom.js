6cb9c7b4008ff74dc21cd9b1025745255302b8db
mehedihasanshamim-star <57722840+mehedihasanshamim-star@users.noreply.github.com>
2020-07-31 04:52:52 +0600

    Add files via upload

---
+++ js/coustom.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	/* -- out- typed js --*/
+	var typed = new Typed('.typing-outo', {
+		strings: [" Eid Mubarak...MehediShamim",
+		],
+		loop:true,
+		backSpeed:100,
+		typeSpeed: 100,
+		backDelay: 900
+	});
+
+});
